cb1def4bc3afab496d6dc9c01a9d68ffa0d0f638
krio <kevinrio@gmail.com>
2010-12-12 23:16:58 -0500

    initial push

---
+++ jquery.krioImageLoader.js
@@ -0,0 +1,47 @@
+/*
+ * Krio Image Loader Jquery Plugin v1
+ * http://krio.me/jquery-image-loader-plugin
+ * http://github.com/jquery-image-loader-plugin
+*/
+
+(function($) {
+  
+	$.fn.krioImageLoader = function(options) {
+    	var opts = $.extend({}, $.fn.krioImageLoader.defaults, options);
+		var imagesToLoad = $(this).find("img")
+									.css({opacity: 0, display: "block", visibility: "hidden"})
+									.addClass("krioImageLoader");
+		var imagesToLoadCount = imagesToLoad.size();
+		alert(imagesToLoadCount);
+		var checkIfLoadedTimer = setInterval(function() {
+			if(!imagesToLoadCount) {
+				clearInterval(checkIfLoadedTimer);
+				return;
+			} else {
+				imagesToLoad.filter(".krioImageLoader").each(function() {
+					if(this.complete) {
+						fadeImageIn(this);
+						imagesToLoadCount--;
+					}
+				});
+			}
+		}, opts.loadedCheckEvery);
+		
+		var fadeImageIn = function(imageToLoad) {
+	    	$(imageToLoad).css({visibility: "visible"})
+						.animate({opacity: 1}, 
+								opts.imageEnterDelay, 
+								removeKrioImageClass(imageToLoad));
+		};
+		
+		var removeKrioImageClass = function(imageToRemoveClass) {
+			$(imageToRemoveClass).removeClass("krioImageLoader");
+		};
+	};
+
+	$.fn.krioImageLoader.defaults = {
+		loadedCheckEvery: 350,
+		imageEnterDelay: 900
+	};
+
+})(jQuery);
