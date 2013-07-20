5131f3f27714b41919ffb004601d98938208933f
Kevin Rio <kevinrio@gmail.com>
2013-07-20 17:47:15 -0400

    Added minified version along with some formatting changes in the source.

--- jquery.krioImageLoader.js
+++ jquery.krioImageLoader.js
@@ -2,46 +2,35 @@
  * Krio Image Loader Jquery Plugin v1
  * http://krio.me/jquery-image-loader-plugin
  * http://github.com/jquery-image-loader-plugin
-*/
+ */
+(function ($) {
+    $.fn.krioImageLoader = function (options) {
+        var imagesToLoad = $(this).find("img").css({opacity: 0, visibility: "hidden"}).addClass("krioImageLoader"),
+            imagesToLoadCount = imagesToLoad.size(),
+            opts = $.extend({}, $.fn.krioImageLoader.defaults, options);
 
-(function($) {
+        var checkIfLoadedTimer = setInterval(function () {
+            if (!imagesToLoadCount) {
+                clearInterval(checkIfLoadedTimer);
+            } else {
+                imagesToLoad.filter(".krioImageLoader").each(function () {
+                    if (this.complete) {
+                        fadeImageIn(this);
+                        imagesToLoadCount--;
+                    }
+                });
+            }
+        }, opts.loadedCheckEvery);
 
-	$.fn.krioImageLoader = function(options) {
-    	var opts = $.extend({}, $.fn.krioImageLoader.defaults, options);
-		var imagesToLoad = $(this).find("img")
-									.css({opacity: 0, visibility: "hidden"})
-									.addClass("krioImageLoader");
-		var imagesToLoadCount = imagesToLoad.size();
+        var fadeImageIn = function (imageToLoad) {
+            $(imageToLoad).css({visibility: "visible"}).animate({opacity: 1}, opts.imageEnterDelay, function () {
+                $(imageToLoad).removeClass("krioImageLoader");
+            });
+        };
+    };
 
-		var checkIfLoadedTimer = setInterval(function() {
-			if(!imagesToLoadCount) {
-				clearInterval(checkIfLoadedTimer);
-				return;
-			} else {
-				imagesToLoad.filter(".krioImageLoader").each(function() {
-					if(this.complete) {
-						fadeImageIn(this);
-						imagesToLoadCount--;
-					}
-				});
-			}
-		}, opts.loadedCheckEvery);
-		
-		var fadeImageIn = function(imageToLoad) {
-			$(imageToLoad).css({visibility: "visible"})
-							.animate({opacity: 1}, 
-								opts.imageEnterDelay, 
-								removeKrioImageClass(imageToLoad));
-		};
-		
-		var removeKrioImageClass = function(imageToRemoveClass) {
-			$(imageToRemoveClass).removeClass("krioImageLoader");
-		};
-	};
-
-	$.fn.krioImageLoader.defaults = {
-		loadedCheckEvery: 350,
-		imageEnterDelay: 900
-	};
-
+    $.fn.krioImageLoader.defaults = {
+        loadedCheckEvery: 350,
+        imageEnterDelay: 300
+    };
 })(jQuery);
